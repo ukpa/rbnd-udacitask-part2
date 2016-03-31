dd4c594aae470794ea754a61359cdeff6420c838
Unnikrishnan Patel <patel.unnikrishnan@gmail.com>
2016-03-31 13:03:58 +0530

    Refactored the module

--- lib/listable.rb
+++ lib/listable.rb
@@ -1,3 +1,25 @@
 module Listable
   # Listable methods go here
+  def format_description(description)
+    "#{description}".ljust(30)
+  end
+
+  def format_priority(priority)
+    value = " ⇧" if priority == "high"
+    value = " ⇨" if priority == "medium"
+    value = " ⇩" if priority == "low"
+    value = "" if !priority
+    return value
+  end
+
+  def format_date(*date)
+    if date.length ==1
+      return  date[0] ? date.strftime("%D") : "No due date"
+    else
+      dates = date[0].strftime("%D") if date[0]
+      dates << " -- " + date[1].strftime("%D") if date[1]
+      dates = "N/A" if !dates
+      return dates
+    end
+  end
 end
--- lib/todo.rb
+++ lib/todo.rb
@@ -1,5 +1,5 @@
 class TodoItem
-  # include Listable
+  include Listable
   attr_reader :description, :due, :priority
 
   def initialize(description, options={})
@@ -7,22 +7,13 @@ class TodoItem
     @due = options[:due] ? Date.parse(options[:due]) : options[:due]
     @priority = options[:priority]
   end
-  def format_description
-    "#{@description}".ljust(25)
-  end
   def format_date
     @due ? @due.strftime("%D") : "No due date"
   end
-  def format_priority
-    value = " ⇧" if @priority == "high"
-    value = " ⇨" if @priority == "medium"
-    value = " ⇩" if @priority == "low"
-    value = "" if !@priority
-    return value
-  end
+
   def details
-    format_description + "due: " +
+    format_description(@description) + "due: " +
     format_date +
-    format_priority
+    format_priority(priority)
   end
 end
